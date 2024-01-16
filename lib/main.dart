9657d7e52e19dcf28334978578ba5a64bae1c8ab
docampol <docampol@intergrupo.com>
2024-01-15 22:36:57 -0500

    firebase configurado

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:r5_test/app_colors.dart';
 import 'package:r5_test/auto_route.gr.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDwrWaV70asrC5pPXMXANO7b9BzpG8HElQ",
+          appId: "1:584876190947:android:a83a96a304487cad5ce9d6",
+          messagingSenderId: "584876190947",
+          projectId: "r5-test-5f7d7")); // Inicializa Firebase
   runApp(MyApp());
 }
 
--- lib/presentation/views/home_view.dart
+++ lib/presentation/views/home_view.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +7,40 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(body: Column(children: [Text("data")]),));
+    
+    Map<String, dynamic> misDatos = {
+      "title": "Ejemplo de título",
+      "description": "Ejemplo de descripción",
+      "status": "Ejemplo de estado",
+      "date": "Ejemplo de fecha",
+    };
+    return SafeArea(
+        child: Scaffold(
+      body: Column(children: [
+        GestureDetector(
+          child: Text("data"),
+          onTap: () {
+            guardarDatosEnFirestore(misDatos);
+          },
+        )
+      ]),
+    ));
+  }
+
+  void guardarDatosEnFirestore(Map<String, dynamic> data) async {
+     WidgetsFlutterBinding.ensureInitialized();
+  await  Firebase.initializeApp();
+    // Datos a guardar
+    try {
+      // Inicializa Firebase
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      // Agrega un nuevo documento a la colección "tu_coleccion" con los datos proporcionados
+      await firestore.collection('tu_coleccion').add(data);
+
+      print('Datos guardados exitosamente en Cloud Firestore');
+    } catch (error) {
+      print('Error al guardar datos en Cloud Firestore: $error');
+    }
   }
 }
